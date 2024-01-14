526f126b616e9a982805c93ffca07280baafeb43
Qwerty000101 <andreyiv20005@gmail.com>
2024-01-15 02:42:02 +0300

    For the balance of the Universe

--- programs/levenstein.py
+++ programs/levenstein.py
@@ -2,51 +2,94 @@
 # -*- coding: utf-8 -*-
 
 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr, 0
-
-    mid = len(arr) // 2
-    left, inv_left = merge_sort(arr[:mid])
-    right, inv_right = merge_sort(arr[mid:])
-    merged, inv_merge = merge(left, right)
-
-    return merged, (inv_left + inv_right + inv_merge)
+import math
+import sys
 
 
-def merge(left, right):
-    i, j = 0, 0
-    inversions = 0
-    merged = []
+def edit_dist(a, b, len_a, len_b):
+    def edit_dist_td(i, j):
+        if D[i][j] == inf:
+            if i == 0:
+                D[i][j] = j
 
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            merged.append(left[i])
-            i += 1
-        else:
-            merged.append(right[j])
-            j += 1
-            inversions += len(left) - i
+            elif j == 0:
+                D[i][j] = i
 
-    merged += left[i:]
-    merged += right[j:]
+            else:
+                ins = edit_dist_td(i, j-1) + 1
+                delete = edit_dist_td(i-1, j) + 1
+                sub = edit_dist_td(i-1, j-1) + (a[i-1] != b[j-1])
+                D[i][j] = min(ins, delete, sub)
 
-    return merged, inversions
+        return D[i][j]
 
 
-if __name__ == "__main__":
-    arr = [1, 3, 5, 2, 4, 6, 100, 99, 33, 29, 35]
-    print(f"Массив: {arr}")
+    def restore():
+        str_first_r, str_second_r = [], []
+        i, j = len_a, len_b
+        while (i, j) != (0, 0):
+            if i != 0 and D[i][j] == D[i-1][j] + 1:
+                str_first_r.append(a[i-1])
+                str_second_r.append('-')
+                i -= 1
 
-    sorted_arr, inversions = merge_sort(arr)
-    print(f"Отсортированный массив: {sorted_arr}")
-    print(f"Количество инверсий: {inversions}")
+            elif j != 0 and D[i][j] == D[i][j-1] + 1:
+                str_first_r.append('-')
+                str_second_r.append(b[j-1])
+                j -= 1
+
+            elif D[i][j] == D[i-1][j-1] + (a[i-1] != b[j-1]):
+                str_first_r.append(a[i-1])
+                str_second_r.append(b[j-1])
+                i -= 1
+                j -= 1
+
+        str_first_r.reverse()
+        str_second_r.reverse()
+
+        return (str_first_r, str_second_r)
 
 
+    def edit_dist_bu():
+        D = []
+
+        for i in range(len_a+1):
+            D.append([i])
+
+        for j in range(1, len_b+1):
+            D[0].append(j)
+
+        for i in range(1, len_a+1):
+            for j in range(1, len_b+1):
+                c = a[i-1] != b[j-1]
+                D[i].append(min(D[i-1][j] + 1,D[i][j-1] + 1,D[i-1][j-1] + c))
+                
+        return D
 
 
+    inf = math.inf
+    D = [[inf] * (len_b+1) for _ in range(len_a+1)]
+    edit_first, edit_second = edit_dist_td(len_a, len_b), edit_dist_bu()
+    result = restore()
+
+    if D == edit_second:
+        return edit_first, result
+    
+    else:
+        print("Ошибка", file=sys.stderr())
+        exit(1)
 
 
+if __name__ == '__main__':
+    str_first = "Австрия"
+    str_second = "Австралия"
+    edit, result = edit_dist(
+        str_first, 
+        str_second, 
+        len(str_first), 
+        len(str_second)
+    )
 
-
-
+    print(f"Расстояние: {edit}\nСтроки:")
+    for item in result:
+        print(*item)
